1e7524f6a5f7d6b11a58ff957a997726b56aed4d
Constance Crozier <Constance@ConstancesMBP3.lan>
2019-10-22 21:36:26 +0100

    no message

--- hydrogen.py
+++ hydrogen.py
@@ -3,57 +3,120 @@ import matplotlib.pyplot as plt
 import numpy as np
 import copy
 
+petrol_C02 = 350
+petrol_kWh = 1.04
 
 e = [42,22,40,1,-11,-80,46,12]
 g = [54,38,52,16,-21,-62,100,100]
 
-plt.figure(figsize=(8,3))
+y1 = [petrol_C02]
+y2 = [petrol_kWh]
+
+for i in range(len(g)):
+    y1.append(y1[0]*(1-g[i]/100))
+    y2.append(y2[0]*(1-e[i]/100))
+    
 plt.rcParams["font.family"] = 'serif'
 plt.rcParams['font.size'] = 11
-plt.bar(np.arange(0,8)-0.2,e,width=0.4,zorder=2,label='Energy',edgecolor='black',)
-plt.bar(np.arange(0,8)+0.2,g,width=0.4,zorder=2,label='Emissions',edgecolor='black', hatch="//")
-plt.xticks(range(8),['Natural Gas\n(Central,G)','Natural Gas\n(Central,L)',
-                     'Natural Gas\n(Station,G)','Natural Gas\n(Station,L)',
-                     'Electrolysis\n(US Mix,G)','Electrolysis\n(US Mix,L)',
-                     'Electrolysis\n(Renew,G)','Electrolysis\n(Renew,L)'],
-           rotation=90)
-plt.grid(zorder=0)
-plt.ylabel('Reduction compared to\npetrol vehicles (%)')
-plt.legend()
-plt.tight_layout()
+#plt.figure(figsize=(8,3))
+fig, ax1 = plt.subplots(figsize=(8,4))
+
+plt.xticks(rotation=90)
+ax1.set_ylabel('Energy Consumption kWh/mile', color='#1f77b4')
+ax1.bar(np.arange(0,9)-0.2,y2,width=0.4,zorder=2,edgecolor='black',)
+ax1.plot([-1,10],[petrol_kWh,petrol_kWh],ls=':',c='#1f77b4')
+ax1.tick_params(axis='y', labelcolor='#1f77b4')
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+
+ax2.set_ylabel('GHG emissions ($C0_2$e g/mile)', color='#ff7f0e')  # we already handled the x-label with ax1
+ax2.bar(np.arange(0,9)+0.2,y1,width=0.4,zorder=2,label='Emissions',edgecolor='black', hatch="//",color='#ff7f0e')
+ax2.tick_params(axis='y', labelcolor='#ff7f0e')
+
+ax2.plot([-1,10],[petrol_C02,petrol_C02],ls=':',c='#ff7f0e')
+plt.xticks(range(9),['Petrol\nICE','$H_2$-Natural Gas\n(Central,G)','$H_2$-Natural Gas\n(Central,L)',
+                     '$H_2$-Natural Gas\n(Station,G)','$H_2$-Natural Gas\n(Station,L)',
+                     '$H_2$-Electrolysis\n(US Mix,G)','$H_2$-Electrolysis\n(US Mix,L)',
+                     '$H_2$-Electrolysis\n(Renew,G)','$H_2$-Electrolysis\n(Renew,L)'])
+
+plt.xlim(-0.8,8.8)
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.savefig('hydrogen.pdf',format='pdf',dpi=300)
 
 
 
 
 
-plt.figure(figsize=(5.8,2.6))
 plt.rcParams['font.size'] = 12
 e = [-11,-18,-6]
 g = [82.5,91,14]
-plt.bar(np.arange(0,3)-0.2,e,width=0.4,zorder=2,label='Energy',edgecolor='black',)
-plt.bar(np.arange(0,3)+0.2,g,width=0.4,zorder=2,label='Emissions',edgecolor='black', hatch="//")
-plt.xticks(range(3),['CBG\n(TG)','CBG\n(AG)','CNG'],rotation=90)
-plt.plot([0.2,0.2],[75,90],c='k',lw=3)
-plt.plot([1.2,1.2],[83,99],c='k',lw=3)
-plt.grid(zorder=0)
-plt.ylabel('Reduction compared to\npetrol vehicles (%)')
-plt.legend()
-plt.tight_layout()
+
+y1 = [petrol_C02]
+y2 = [petrol_kWh]
+
+for i in range(len(g)):
+    y1.append(y1[0]*(1-g[i]/100))
+    y2.append(y2[0]*(1-e[i]/100))
+
+
+fig, ax1 = plt.subplots(figsize=(5.8,3.5))
+
+plt.xticks(rotation=90)
+ax1.set_ylabel('Energy Consumption kWh/mile', color='#1f77b4')
+ax1.bar(np.arange(0,4)-0.2,y2,width=0.4,zorder=2,edgecolor='black',)
+ax1.plot([-1,10],[petrol_kWh,petrol_kWh],ls=':',c='#1f77b4')
+ax1.tick_params(axis='y', labelcolor='#1f77b4')
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+
+ax2.set_ylabel('GHG emissions ($C0_2$e g/mile)', color='#ff7f0e')  # we already handled the x-label with ax1
+ax2.bar(np.arange(0,4)+0.2,y1,width=0.4,zorder=2,label='Emissions',edgecolor='black', hatch="//",color='#ff7f0e')
+ax2.tick_params(axis='y', labelcolor='#ff7f0e')
+
+ax2.plot([-1,10],[petrol_C02,petrol_C02],ls=':',c='#ff7f0e')
+plt.xticks(range(4),['Petrol\nICE','CBG\n(TG)','CBG\n(AG)','CNG'])
+
+plt.xlim(-0.7,3.7)
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.savefig('biogas.pdf',format='pdf',dpi=300)
 
-plt.figure(figsize=(5.8,3))
-plt.rcParams['font.size'] = 12
+
+
+
+
 e = [64]*4
 g = [28,89,-3,6]
-plt.bar(np.arange(0,4)-0.2,e,width=0.4,zorder=2,label='Energy',edgecolor='black',)
-plt.bar(np.arange(0,4)+0.2,g,width=0.4,zorder=2,label='Emissions',edgecolor='black', hatch="//")
-plt.xticks(range(4),['US\nMix','French\nMix','China\nMix','India\nMix'],rotation=90)
-plt.grid(zorder=0)
-plt.ylabel('Reduction compared to\npetrol vehicles (%)')
-plt.ylim(-10,105)
-plt.legend()
-plt.tight_layout()
-plt.savefig('electric.pdf',format='pdf',dpi=300)
 
+y1 = [petrol_C02]
+y2 = [petrol_kWh]
+
+for i in range(len(g)):
+    y1.append(y1[0]*(1-g[i]/100))
+    y2.append(y2[0]*(1-e[i]/100))
+
+
+fig, ax1 = plt.subplots(figsize=(5.8,4))
+
+plt.xticks(rotation=90)
+ax1.set_ylabel('Energy Consumption kWh/mile', color='#1f77b4')
+ax1.bar(np.arange(0,5)-0.2,y2,width=0.4,zorder=2,edgecolor='black',)
+ax1.plot([-1,10],[petrol_kWh,petrol_kWh],ls=':',c='#1f77b4')
+ax1.tick_params(axis='y', labelcolor='#1f77b4')
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+
+ax2.set_ylabel('GHG emissions ($C0_2$e g/mile)', color='#ff7f0e')  # we already handled the x-label with ax1
+ax2.bar(np.arange(0,5)+0.2,y1,width=0.4,zorder=2,label='Emissions',edgecolor='black', hatch="//",color='#ff7f0e')
+ax2.tick_params(axis='y', labelcolor='#ff7f0e')
+
+ax2.plot([-1,10],[petrol_C02,petrol_C02],ls=':',c='#ff7f0e')
+plt.xticks(range(5),['Petrol\nICE','EV-US\nMix','EV-French\nMix','EV-China\nMix','EV-India\nMix'])
+
+plt.xlim(-0.7,4.7)
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.savefig('electric.pdf',format='pdf',dpi=300)
 plt.show()
+
